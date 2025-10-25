814b5883ebca2857bf7bae298eb4bed2eac54421
FelipePetra <felipe.petra@yahoo.com.br>
2025-10-24 21:44:26 -0300

    Add files via upload

--- trabalho python.py
+++ trabalho python.py
@@ -2,17 +2,7 @@ import tkinter
 import PyPDF2
 import requests
 
-url = "https://date.nager.at/api/v3/PublicHolidays/2024/BR"
 
-payload = {}
-headers = {
-  'accept': 'application/json',
-  'X-CSRF-TOKEN': 'pYBqfz7tfH5NFeqA2YXNhdZIsqRCMmef6FjOTNJz'
-  }
-
-response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
 
 url = "https://date.nager.at/api/v3/PublicHolidays/2025/BR"
 
@@ -26,22 +16,20 @@ response = requests.request("GET", url, headers=headers, data=payload)
 
 print(response.text)
 
-url = "https://date.nager.at/api/v3/PublicHolidays/2026/BR"
 
-payload = {}
-headers = {
-  'accept': 'application/json',
-  'X-CSRF-TOKEN': 'pYBqfz7tfH5NFeqA2YXNhdZIsqRCMmef6FjOTNJz'
-  }
 
-response = requests.request("GET", url, headers=headers, data=payload)
 
-print(response.text)
 
 
 def Get_text_from_PDFfiles_usingPyPDF2(in_PdfFile):
     reader = PyPDF2.PdfReader(in_PdfFile) 
     print(reader.pages[0].extract_text())
+    lista_de_datas = reader.pages[0].extract_text().split("\n")
+    print (lista_de_datas)
+    for data in lista_de_datas:
+        if data.strip() in response.text:
+            print (data)
+        
 
 def Exemplo():
     root = tkinter.Tk()
