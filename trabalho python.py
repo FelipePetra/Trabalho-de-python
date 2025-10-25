72f4b64c248455e515d9b37f471dbb4b2f920693
FelipePetra <felipe.petra@yahoo.com.br>
2025-10-24 21:01:12 -0300

    Add files via upload

--- trabalho python.py
+++ trabalho python.py
@@ -2,6 +2,18 @@ import tkinter
 import PyPDF2
 import requests
 
+url = "https://date.nager.at/api/v3/PublicHolidays/2024/BR"
+
+payload = {}
+headers = {
+  'accept': 'application/json',
+  'X-CSRF-TOKEN': 'pYBqfz7tfH5NFeqA2YXNhdZIsqRCMmef6FjOTNJz'
+  }
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
 url = "https://date.nager.at/api/v3/PublicHolidays/2025/BR"
 
 payload = {}
@@ -14,7 +26,20 @@ response = requests.request("GET", url, headers=headers, data=payload)
 
 print(response.text)
 
-def Get_text_from_PDFfiles_usingPyPDF2(in_PdfFile):  
+url = "https://date.nager.at/api/v3/PublicHolidays/2026/BR"
+
+payload = {}
+headers = {
+  'accept': 'application/json',
+  'X-CSRF-TOKEN': 'pYBqfz7tfH5NFeqA2YXNhdZIsqRCMmef6FjOTNJz'
+  }
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
+
+def Get_text_from_PDFfiles_usingPyPDF2(in_PdfFile):
     reader = PyPDF2.PdfReader(in_PdfFile) 
     print(reader.pages[0].extract_text())
 
@@ -23,15 +48,14 @@ def Exemplo():
     root.title("Leitor de PDF")
     root.resizable(True, True)
     
-    test = tkinter.Button(root, text="Ler PDF")
-    test['command'] = LerPDF
+    test = tkinter.Button(root, text="Ler PDF", command=LerPDF)
     test.pack()
 
     
     root.mainloop()
 
 def LerPDF():
-    Get_text_from_PDFfiles_usingPyPDF2(r"C:\Users\202502304706\Downloads\Trabalho.zip\Trabalho")
+    Get_text_from_PDFfiles_usingPyPDF2(r"C:\Users\202502304706\Documents\Lista de datas.pdf")
 
 Exemplo()
 LerPDF()
